62387e5960575a7fc52cb7437a5ee99451b63817
Irene Papakonstantinou <eirhnh@gmail.com>
2017-12-03 02:03:42 +0000

    Day 2

---
+++ README.md
@@ -0,0 +1,3 @@
+Advent of Code
+
+http://adventofcode.com/
---
+++ src/2.hs
@@ -0,0 +1,14 @@
+module Main where
+import Data.List
+
+main :: IO ()
+main = do
+  ls <- lines <$> readFile  "input2.txt"
+  let is = map (map (\ s -> read s :: Int) . words) ls
+  print $ checksum1 is
+  print $ checksum2 is
+
+
+checksum1 = sum . map (\l -> maximum l - minimum l)
+
+checksum2 = sum . map (\l -> head [ x `div` y | x <- l, y <- l, x/=y, x `mod` y == 0])
