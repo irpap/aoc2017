3aac41b95d5936f15ca5b0df06ca8eb227485102
Irene Papakonstantinou <eirhnh@gmail.com>
2017-12-05 23:42:57 +0000

    Day 5 part 2

--- src/5.hs
+++ src/5.hs
@@ -6,10 +6,13 @@ import Data.Sequence (Seq, fromList, index, update)
 main :: IO ()
 main = do
   xs <- (map (read::String->Int) . lines) <$> readFile  "inputs/input5.txt"
-  print $ steps 0 0 (fromList xs)
+  print $ steps 0 0 (+1) (fromList xs)
+  print $ steps 0 0 (\x -> if x >= 3 then x-1 else x+1) (fromList xs)
 
-steps:: Int -> Int -> Seq Int -> Int
-steps n i s
+
+steps:: Int -> Int -> (Int->Int) -> Seq Int -> Int
+steps n i f s
   | i < 0 || i >= length s = n
-  | otherwise = steps (n+1) (i+v) (update i (v+1) s)
+  | otherwise = steps (n+1) (i+v) f (update i (f v) s)
       where v = index s i
+ 
